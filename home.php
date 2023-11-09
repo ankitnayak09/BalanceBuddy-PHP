acb9c667b41ec6721982a970f087bee13103c553
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-11-09 16:46:20 +0530

    Login Page Done

---
+++ home.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+  <title>Balance Buddy</title>
+
+  <style>
+    /* Custom CSS */
+    body {
+      background-color: #22223B;
+    }
+
+    .hero-div {
+      background-image: url("img/hero.jpg");
+      height: 500px;
+      overflow: hidden;
+    }
+  </style>
+</head>
+
+<body>
+  <?php
+  include 'navbar.php';
+  ?>
+
+  <div class="container-fluid">
+    <!-- Introduction section -->
+    <div class="row intro">
+
+      <div class="col-sm-12 col-md img text-center hero-div">
+        <!-- TODO: Replace Hero Image -->
+        <!-- <img src="img/bank.png" class="img-fluid pt-2"> -->
+        <!-- <img src="img/hero.jpg" class="img-fluid pt-2"> -->
+      </div>
+    </div>
+
+    <!-- Activity section -->
+    <div class="row activity text-center py-5">
+      <div class="col-md act">
+        <img src="img/user.jpg" class="img-fluid my-2" style="border-radius: 50%">
+        <br>
+        <a href="createuser.php"><button style="background-color : #2785C4;" style="border-radius:0%">Create a
+            User</button></a>
+      </div>
+      <div class="col-md act">
+        <img src="img/ruser.jpg" class="img-fluid my-2" style="border-radius: 50%">
+        <br>
+        <a href="removeuser.php"><button style="background-color : #2785C4;" style="border-radius:0%">Delete
+            Users</button></a>
+      </div>
+      <div class="col-md act">
+        <img src="img/transfer.jpg" class="img-fluid my-2" style="border-radius: 50%">
+        <br>
+        <a href="transfermoney.php"><button style="background-color : #2785C4;">Make a Transaction</button></a>
+      </div>
+      <div class="col-md act">
+        <img src="img/history.jpg" class="img-fluid my-2" style="border-radius: 50%">
+        <br>
+        <a href="transactionhistory.php"><button style="background-color : #2785C4;">Transaction History</button></a>
+      </div>
+
+    </div>
+  </div>
+  <footer class="text-center mt-5 py-2">
+    <p>Developed by <a href="#">
+        <b>Ankit Kumar Nayak</b>
+      </a>
+    </p>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background-color: #22223b;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+        }
+
+        h1 {
+            font-family: 'Mono';
+            color: #f2e9e4;
+            text-align: center
+        }
+
+        main {
+            background-color: #4a4e69;
+            border-radius: 20px;
+            padding: 50px;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+            margin-bottom: 20px;
+        }
+
+        input {
+            padding: 5px 10px;
+        }
+
+        button {
+            width: 100%;
+            padding: 5px 0;
+            cursor: pointer;
+            transition: 0.5s;
+            font-weight: bold;
+
+        }
+
+        input,
+        button {
+            border-radius: 5px;
+            border: none;
+            font-size: 1.5rem;
+        }
+
+        button:hover {
+            transform: scale(1.05);
+            background-color: #22223B;
+            color: #F2E9E4;
+        }
+
+        button:active {
+            transform: scale(0.95);
+        }
+
+        .hr {
+            background-color: #F2E9E4;
+            height: 1px;
+            width: 100%;
+            margin: 0 auto;
+        }
+
+        div.signup-links {
+            color: #F2E9E4;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+</body>
+<main>
+    <form action="/login.php">
+        <h1>Balance Buddy</h1>
+        <input type="email" placeholder="Email..." required>
+        <input type="password" placeholder="Password..." required>
+        <button type="submit">Login</button>
+    </form>
+    <div class="hr"></div>
+    <div class="text-center signup-links">Don't Have an account ? <a href="signup.php">Create One</a></div>
+</main>
+
+</html>
