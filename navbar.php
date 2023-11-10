3792b9847ca57f5d84b6dc11b5f6292e9e5405d2
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-11-10 15:15:25 +0530

    add users page to navbar

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <!-- navbar -->
 <nav class="navbar navbar-expand-md navbar-light">
   <!-- TODO: Add WebApp Logo -->
-  <a class="navbar-brand" href="home.php" style="color: #F2E9E4;">
+  <a class="navbar-brand" href="index.php" style="color: #F2E9E4;">
     <!-- <img src="img/bnklg.png" alt="Logo" width="26" style="margin-bottom:4px;">  -->
     <b>Balance Buddy</b></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -10,22 +10,18 @@
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a class="nav-link" href="home.php" style="color: #F2E9E4;"><b>Home</b></a>
+        <a class="nav-link" href="index.php" style="color: #F2E9E4;"><b>Home</b></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="createuser.php" style="color: #F2E9E4;"><b>Create User</b></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="removeuser.php" style="color: #F2E9E4;"><b>Remove User</b></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="transfermoney.php" style="color: #F2E9E4;"><b>Transfer Money</b></a>
-      </li>
+      <?php if ($_SESSION["username"] == "admin") { ?>
+        <li class="nav-item">
+          <a class="nav-link" href="users.php" style="color: #F2E9E4;"><b>All Users</b></a>
+        </li>
+      <?php } ?>
       <li class="nav-item">
         <a class="nav-link" href="transactionhistory.php" style="color: #F2E9E4;"><b>Transaction History</b></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="index.php" style="color: #F2E9E4;"><b>Logout</b></a>
+        <a class="nav-link" href="logout.php" style="color: #F2E9E4;"><b>Logout</b></a>
       </li>
   </div>
 </nav>
